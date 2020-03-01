77c41b77cc95ffc36f7db58ba3984b15514c56b1
nadeenamr <nadeen.riad@student.guc.edu.eg>
2020-03-02 01:52:50 +0200

    courses prereq bug 4

--- courses.js
+++ courses.js
@@ -30,6 +30,7 @@ module.exports = {
                             callback(prereqCourses);
                         };
                     });
+            done();
         });
         pool.end();
     }   
