0cd5c8389a56adaabee0f7f334a5bda5538fc51d
nadeenamr <nadeen.riad@student.guc.edu.eg>
2020-03-01 21:53:52 +0200

    get all colors string bug 5

--- colors.js
+++ colors.js
@@ -13,7 +13,7 @@ module.exports = {
                 return console.error('Error acquiring client', err.stack);
             }
             client.query(
-                    `SELECT color FROM public.iphone_colors`,
+                    'SELECT color FROM public.iphone_colors',
                     function(err, result) {
                         if (err) {
                             console.log(err);
