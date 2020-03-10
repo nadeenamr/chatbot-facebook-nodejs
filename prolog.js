a9c918ff97cbd8524399a905f368817f38f21b48
nadeenamr <nadeen.riad@student.guc.edu.eg>
2020-03-10 19:25:52 +0200

    tried making intent that uses prolog bug 8

--- prolog.js
+++ prolog.js
@@ -10,7 +10,7 @@ require( "tau-prolog/modules/lists" )( pl ); // Import and apply the lists modul
 module.exports = {
 
   getStudentNextSchedule: function(callback, studentID) {
-    var scheduleQuery = "getSchedule("+studentID+",X).";
+    var scheduleQuery = "getSchedule($1,X).";
     var outputSchedule = executeQuery(scheduleQuery);
     callback(outputSchedule);                         
   }
