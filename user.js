60d45560c5d27b644841151882ed4ee0f2df732a
nadeenamr <nadeen.riad@student.guc.edu.eg>
2020-03-17 18:46:35 +0200

    trying different code methods bug 7

--- user.js
+++ user.js
@@ -26,17 +26,13 @@ module.exports = function(callback, userId){
                             return console.error('Error acquiring client', err.stack);
                         }
                         var rows = [];
-                        console.log('fetching user');
-                        client.query(`SELECT id FROM users WHERE facebook_id='${userId}' LIMIT 1`,
+                        client.query(`SELECT facebook_id FROM users WHERE facebook_id='${userId}' LIMIT 1`,
                             function(err, result) {
-                                console.log('query result ' + result);
                                 if (err) {
                                     console.log('Query error: ' + err);
                                 } else {
-                                    console.log('rows: ' + result.rows.length);
                                     if (result.rows.length === 0) {
                                         let sql = 'INSERT INTO users (facebook_id, first_name, last_name) VALUES ($1, $2, $3)';
-                                        console.log('sql: ' + sql);
                                         client.query(sql,
                                             [
                                                 userId,
