ac8af10aee14bbc76d5588a25535adbb1cfa771b
nadeenamr <nadeen.riad@student.guc.edu.eg>
2020-03-30 09:10:08 +0200

    getting last final for next semester schedule bug 2

--- courses.js
+++ courses.js
@@ -148,7 +148,7 @@ module.exports = {
                     maxDateIndex = i;
                 }
             }
-            console.log("CODE == "+courses[maxDateIndex+"  DATE == "+maxDate]);
+            console.log("CODE == "+courses[maxDateIndex]+"  DATE == "+maxDate);
             callback([courses[maxDateIndex],maxDate]);
             
         });
